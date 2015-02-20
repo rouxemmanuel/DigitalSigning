c970185ffff80177a99422a2b21ee352390f14b2
e.roux1@gmail.com <e.roux1@gmail.com@5c2c40af-f2be-cb15-ca42-cd02f093ca8e>
2015-02-20 10:47:36 +0000

    Change image and add waiting message

--- trunk/DigitalSigningShare/src/main/resources/web/components/documentlibrary/sign-action.js
+++ trunk/DigitalSigningShare/src/main/resources/web/components/documentlibrary/sign-action.js
@@ -25,25 +25,35 @@
 		fn: function DL_onActionSign(record)
 		{
 		 var jsNode = record.jsNode,
-		    nodeRef = record.nodeRef,
 		    displayName = record.displayName,
 		    actionUrl = Alfresco.constants.PROXY_URI + "/api/digitalSigning/sign",
 		    maxLabelSize=0;
+		 
+		 var nodesRef = [];
+		 var multiple = false;
+	     if (record instanceof Array) {
+	    	 multiple = true;
+	    	 for (var i=0, ii=record.length ; i<ii ; i++){
+	        	nodesRef.push(record[i].nodeRef);
+	    	 }
+	     } else {
+	        nodesRef.push(record.nodeRef)
+	      }
 
 		 var resultRefs =[];
 		
-		  var currentId = this.id;
+		 var currentId = this.id;
 		
 		 this.modules.sign = new Alfresco.module.SimpleDialog(this.id + "-sign").setOptions(
 		 {
 		    width: "50em",
-		    templateUrl: Alfresco.constants.URL_SERVICECONTEXT + "modules/digitalSigning/sign?displayName=" + encodeURIComponent(displayName) + "&nodeRef=" + encodeURIComponent(nodeRef),
+		    templateUrl: Alfresco.constants.URL_SERVICECONTEXT + "modules/digitalSigning/sign?displayName=" + encodeURIComponent(displayName) + "&nodeRef=" + encodeURIComponent(nodesRef.join()) + "&multiple=" + multiple,
 		    actionUrl: actionUrl,
 		    doSetupFormsValidation:
             {
                fn: function dlA_doSetupFormsValidation(p_form)
                {
-			 	p_form.addValidation(this.id + "-sign-password", Alfresco.forms.validation.mandatory, null, "change");
+			 	p_form.addValidation(this.id + "-sign-password", Alfresco.forms.validation.mandatory, null, "keyup");
 				
 			 	//p_form.addValidation(this.id + "-sign-filterPathView", function(field, args, event, form, silent, message)
                 //{
@@ -86,6 +96,7 @@
 				
 				this.modules.sign.widgets.pathNodeRef = Dom.get(this.id + "-sign-pathNodeRef"); // NodeRef
 				this.modules.sign.widgets.pathView = Dom.get(this.id + "-sign-filterPathView");
+				
 				this.modules.sign.widgets.selectFilterPathButton = Alfresco.util.createYUIButton(this.modules.sign, "selectFilterPath-button", function getLatestDoc_onSelectFilterPath(e)
 				{
 				  if (!this.widgets.filterPathDialog)
@@ -111,7 +122,11 @@
 					YAHOO.Bubbling.on("folderSelected", function (layer, args) {
 					  var obj = args[1];
 					  if (obj !== null) {
-						this.widgets.pathView.innerHTML = obj.selectedFolder.path;
+						if (obj.selectedFolder.siteTitle != null) {
+							this.widgets.pathView.innerHTML = obj.selectedFolder.siteTitle + obj.selectedFolder.path;
+						} else {
+							this.widgets.pathView.innerHTML = obj.selectedFolder.path;
+						}
 						this.widgets.pathNodeRef.value = obj.selectedFolder.nodeRef;
 					  }
 					}, this);
@@ -130,15 +145,30 @@
 		    {
 		       fn: function dlA_onActionDepotCasier_success(response)
 		       {
+		    	   // Hide waiting dialog
+				   if (this.waitDialog)
+				   {
+                     this.waitDialog.destroy();
+                     this.waitDialog = null;
+				   }
+		    	   
 		    	   var responseJSON = Alfresco.util.parseJSON(response.serverResponse.responseText);
 		    	   if (responseJSON.result == "success") {
+		    		   var successMessage = this.msg("message.sign.success");
+			    	   if (nodesRef.length > 1) {
+			    		   successMessage = this.msg("message.sign.success.multiple");
+			    	   }
 		    		   Alfresco.util.PopupManager.displayMessage({
-			    		   text: this.msg("message.sign.success"),
+			    		   text: successMessage,
 			    		   displayTime: 10
 			    	   });
 		    	   } else {
+		    		   var errorMessage = "message.sign.error";
+			    	   if (nodesRef.length > 1) {
+			    		   errorMessage = "message.sign.errors";
+			    	   }
 		    		   Alfresco.util.PopupManager.displayMessage({
-			    		   text: this.msg("message.sign.error", responseJSON.error),
+			    		   text: this.msg(errorMessage, responseJSON.error),
 			    		   displayTime: 10
 			    	   });
 		    	   }
@@ -149,14 +179,42 @@
 		    {
 		       fn: function dlA_onActionDepotCasier_failure(response)
 		       {
+		    	   // Hide waiting dialog
+		    	   if (this.waitDialog)
+				   {
+                     this.waitDialog.destroy();
+                     this.waitDialog = null;
+				   }
+		    	   
 		    	   var responseJSON = Alfresco.util.parseJSON(response.serverResponse.responseText);
+		    	   var errorMessage = "message.sign.error";
+		    	   if (nodesRef.length > 1) {
+		    		   errorMessage = "message.sign.errors";
+		    	   }
 		    	   Alfresco.util.PopupManager.displayMessage({
-		    		   text: this.msg("message.sign.error", responseJSON.error),
+		    		   text: this.msg(errorMessage, responseJSON.error),
 		    		   displayTime: 10
 		    	   });
 		       },
 		       scope: this
-		    }
+		    },
+		    doBeforeFormSubmit :
+	        {
+	            fn: function(form, obj)
+	            {
+	            	var msg = this.msg("sign.processing.document");
+	            	if (multiple) {
+	            		msg = this.msg("sign.processing.documents");
+	            	}
+	            	
+	            	this.waitDialog = Alfresco.util.PopupManager.displayMessage({
+		                text : msg,
+		                spanClass : "wait",
+		                displayTime : 0
+		             });
+	            },
+	            scope: this
+	         }
 		 });
 		 this.modules.sign.show();
 		}
