57a484f786ef5d2d7c439852d1270612af32d197
Emmanuel ROUX <e.roux1@gmail.com>
2015-08-02 09:30:13 +0200

    Initial version

---
+++ DigitalSigningVerifySign/src/main/java/org/alfresco/plugin/digitalSigningVerifySign/GetSignInformations.java
@@ -0,0 +1,47 @@
+package org.alfresco.plugin.digitalSigningVerifySign;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Enumeration;
+
+public class GetSignInformations {
+
+	public static void main(String[] args) {
+		try {
+			final KeyStore ks = KeyStore.getInstance("pkcs12");
+			ks.load(new FileInputStream(args[0]), args[1].toCharArray());
+			
+			final Enumeration<String> aliases = ks.aliases();
+			while(aliases.hasMoreElements()) {
+				final String alias = aliases.nextElement();
+				final X509Certificate c = (X509Certificate) ks.getCertificate(alias);
+				
+				System.out.println("* Certificate info for alias : " + alias);
+				System.out.println("    - Version : " + c.getVersion());
+			    System.out.println("    - Serial number : " + c.getSerialNumber().toString(16));
+			    System.out.println("    - Subjetc DN : " + c.getSubjectDN());
+			    System.out.println("    - Issuer DN : " + c.getIssuerDN());
+			    System.out.println("    - Valide from : " + c.getNotBefore());
+			    System.out.println("    - Valide to : " + c.getNotAfter());
+			    System.out.println("    - Algorithm : " + c.getSigAlgName());
+			}
+		} catch (KeyStoreException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (CertificateException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
